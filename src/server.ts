c6090fe7cb8ff9efbb085137c6dd2c71f6ab29a9
unknown <gmdasilva47@gmail.com>
2023-07-19 22:58:31 -0300

    Correção de erros na autenticação de usuário

--- src/server.ts
+++ src/server.ts
@@ -9,13 +9,23 @@ const app = express();
 //console.log(process.env)
 
 //middleware
+const cors = require('cors');
+
+app.use(function(req, res, next){
+    res.header("Access-Controll-Allow-Origin", "*");
+    res.header("Access-Controll-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+    res.header("Access-Controll-Allow-Methods", "POST, GET, DELETE, OPTIONS");
+    next();
+});
+
+app.use(cors());
+
 app.use(express.json());
 app.use('/user', userRoutes);
 app.use('/videos', videosRoutes);
 
 
-
-
+app.listen(4000);
 // app.get('/users', (request, response) =>{
 //     response.json([{name: 'Paulo', age:22}, {name:'Jose', age: 30} ])
 // })
@@ -28,4 +38,3 @@ app.use('/videos', videosRoutes);
 //     response.status(200).json({sucess: true})
 // })
 
-app.listen(4000);
